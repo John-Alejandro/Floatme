bd06e3299127e8a64a34dfa31a6442678d38b9c1
john alejandro <john.e.alejandro01@gmail.com>
2022-02-18 23:40:10 -0600

    Created seeder file

---
+++ sql/seeder.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS floatme_db;
+
+USE floatme_db;
+
+INSERT INTO user(name, age, dateJoined, dateUpdated)
+VALUES ('John', 21, 110121, 021822),
+       ('Jane', 19, 120121, 021822),
+       ('Jack', 25, 081220, 021822),
+       ('Joe', 30, 010121, 021822),
+       ('Janice', 22, 020120, 021822);
